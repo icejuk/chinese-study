9116d497ce8f40aa1c2afd84f5951d52cc5c02c5
Waranya Kosonkitja <waranya.k@umt.ltd>
2026-07-10 15:16:35 +0700

    เพิ่มบทที่ 13 (这是不是中药) + หน้าสุ่มศัพท์ดึงศัพท์ทุกบทครบ 234 คำ

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker for 鑫越 Chinese Study PWA */
-const CACHE_VERSION = 'v5-2026-07-09';
+const CACHE_VERSION = 'v7-2026-07-10';
 const CACHE_NAME = `xinyue-zh-${CACHE_VERSION}`;
 
 const PRECACHE_URLS = [
